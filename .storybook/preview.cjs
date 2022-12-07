a03802ae24790d108b6c4c022c9a6160ed84702c
Sjaanee <shara.jane@outlook.com>
2022-12-07 02:01:58 -0300

    first commit

---
+++ .storybook/preview.cjs
@@ -0,0 +1,18 @@
+import { themes } from '@storybook/theming'
+
+import '../src/styles/global.css';
+
+import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs: {
+    theme: themes.dark,
+  }
+}
